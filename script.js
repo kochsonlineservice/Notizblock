6d11baefbc6e0d13a9550c77a1015c825652be55
Ralph Koch <deine@email.de>
2026-05-11 09:49:43 +0200

    js function und html first step

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
 import './js/ui.js';
 import './js/storage.js';
+
+
+
+// View notes
+
+/* =========================
+View notes
+========================= */
+let notes = ['banana', 'rasen mähen'];
+
+function renderNotes() {
+
+    let contentRef = document.getElementById("content");
+
+  for (let  i  = 0;  i  < array.length;  i ++) {
+    const element = array[ i ];
+      contentRef.innerHTML = notes;
+  }
+}
+
+renderNotes();
+
+
+
+// add notes
+// delete notes
+// archives notes
