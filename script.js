7dff937c18b75f09c4fb4f938032069c553abd74
Ralph Koch <deine@email.de>
2026-05-11 16:41:52 +0200

    delete button added

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
 import "./js/ui.js";
 import "./js/storage.js";
+import "./js/ui.js";
+import "./js/storage.js";
 
 /* =========================
 View notes
 ========================= */
-let notes = ["banana", "rasen mähen"];
+
+let notes = ["banana", "kartoffeln", "apfel", "Kiwi"];
 
 window.renderNotes = function () {
+
   let contentRef = document.getElementById("content");
+
   contentRef.innerHTML = "";
 
   for (let indexNote = 0; indexNote < notes.length; indexNote++) {
-    const note = notes[indexNote];
 
-    contentRef.innerHTML += getNoteTemplate(note);
+    contentRef.innerHTML += getNoteTemplate(indexNote);
   }
 };
 
-function getNoteTemplate(note) {
-  return `<p class="note">${note}</p>`;
+function getNoteTemplate(indexNote) {
+
+  return `
+    <p class="note">
+      ${notes[indexNote]}
+      <button class="delete" onclick="deleteNote(${indexNote})">
+        X
+      </button>
+    </p>
+  `;
 }
 
 /* =========================
@@ -26,9 +38,14 @@ add notes
 ========================= */
 
 window.addNotes = function () {
+
   let noteInputRef = document.getElementById("note_Input");
 
   let noteInput = noteInputRef.value;
+
+  if (noteInput.trim() == "") {
+    return;
+  }
 
   notes.push(noteInput);
 
@@ -37,5 +54,13 @@ window.addNotes = function () {
   noteInputRef.value = "";
 };
 
-// delete notes
-// archives notes
+/* =========================
+delete notes
+========================= */
+
+window.deleteNote = function (indexNote) {
+
+  notes.splice(indexNote, 1);
+
+  renderNotes();
+};
