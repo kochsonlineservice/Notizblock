305173f80fec0fa5f76a6312ada52c4a28e44452
Ralph Koch <deine@email.de>
2026-05-15 15:27:16 +0200

    js local storage

--- script.js
+++ script.js
@@ -1,13 +1,59 @@
 import "./js/ui.js";
 import "./js/storage.js";
-import "./js/ui.js";
-import "./js/storage.js";
+
+
 
 /* =========================
 View notes
 ========================= */
+let notesTitle = []
+let notes = [];
+let trashNotes = [];
 
-let notes = ["banana", "kartoffeln", "apfel", "Kiwi"];
+
+
+/* =========================
+local storage
+========================= */
+
+function saveToLocalStorage() {
+
+  localStorage.setItem("notes", JSON.stringify(notes));
+
+  localStorage.setItem("notesTitle", JSON.stringify(notesTitle));
+
+  localStorage.setItem("trashNotes", JSON.stringify(trashNotes));
+
+}
+
+
+function loadFromLocalStorage() {
+
+  let storedNotes = localStorage.getItem("notes");
+
+  let storedTitles = localStorage.getItem("notesTitle");
+
+  let storedTrash = localStorage.getItem("trashNotes");
+
+
+  if (storedNotes) {
+    notes = JSON.parse(storedNotes);
+  }
+
+  if (storedTitles) {
+    notesTitle = JSON.parse(storedTitles);
+  }
+
+  if (storedTrash) {
+    trashNotes = JSON.parse(storedTrash);
+  }
+
+}
+
+/* =========================
+Render  notes
+========================= */
+
 
 window.renderNotes = function () {
 
@@ -21,17 +67,62 @@ window.renderNotes = function () {
   }
 };
 
+
+/* =========================
+Render trash notes
+========================= */
+
+window.renderTrashNotes = function () {
+
+  let trashContentRef = document.getElementById("trash_content");
+
+  trashContentRef.innerHTML = "";
+
+  for (let indextrashNote = 0; indextrashNote < trashNotes.length; indextrashNote++) {
+
+   trashContentRef.innerHTML += getTrashTemplate(indextrashNote);
+  }
+};
+
+
+/* =========================
+Note template
+========================= */
+
 function getNoteTemplate(indexNote) {
 
   return `
-    <p class="note">
-      ${notes[indexNote]}
-      <button class="delete" onclick="deleteNote(${indexNote})">
+    <p class="note">  ${notesTitle [indexNote]} = ${notes[indexNote]}
+
+      <button class="delete"
+       onclick="deleteNote(${indexNote})">
         X
       </button>
     </p>
   `;
 }
+
+
+/* =========================
+Trash template
+========================= */
+
+function getTrashTemplate(indextrashNote) {
+
+  return `
+    <p class="note">
+     ${trashNotes[indextrashNote]}
+      
+     <button class="delete"
+       onclick="deleteTrashNotes(${indextrashNote})">
+        X
+      </button>
+
+    </p>
+  `;
+}
+
+
 
 /* =========================
 add notes
@@ -49,18 +140,65 @@ window.addNotes = function () {
 
   notes.push(noteInput);
 
+  notesTitle.push(`Nummer ${notesTitle.length + 1}`);
+
   renderNotes();
+
+  saveToLocalStorage();
 
   noteInputRef.value = "";
 };
 
+
+
 /* =========================
 delete notes
 ========================= */
-
 window.deleteNote = function (indexNote) {
 
-  notes.splice(indexNote, 1);
+    let deletedNote = notes[indexNote];
 
-  renderNotes();
+    trashNotes.push(deletedNote);
+
+    notes.splice(indexNote, 1);
+
+    notesTitle.splice(indexNote, 1);
+
+    saveToLocalStorage();
+
+    renderNotes();
+
+    renderTrashNotes();
 };
+/* =========================
+delete trashnotes
+========================= */
+
+window.deleteTrashNotes = function (indextrashNote) {
+
+    trashNotes.splice(indextrashNote, 1);
+
+    saveToLocalStorage();
+
+    window.renderTrashNotes();
+};
+
+/* =========================
+local storage rendern
+========================= */
+     loadFromLocalStorage();
+
+        renderNotes();
+        renderTrashNotes();
+
+
+/* =========================
+Enter onkey push
+========================= */
+
+window.handleEnter = function(event){
+
+    if(event.key === "Enter"){
+        addNotes();
+    }
+}
